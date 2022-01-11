d6ed95a563f05c19b6cc8616d61f13e9688118c2
udaybhang <guptaudaybhan02@gmail.com>
2022-01-11 19:59:45 +0530

    swagger added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
-
+const swaggerJDDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 const dbConfig = require('./config/connection');
 const mongoose = require('mongoose');
 app.use(bodyParser.json())
@@ -15,6 +16,27 @@ mongoose.connect(dbConfig.url, {
 });
 
 require('./routes/user_routes')(app);
+const swaggerDocument = require('./swagger.json');
+const swaggerOptions = {
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Managment System',
+            version:'1.0.0',
+            description:'api for documentation',
+            contact:{
+                name:'udaybhan',
+                url:'https://app.com',
+                email:'app@gmail.com'
+            },
+            servers:['http://localhost:3000']
+        }
+    },
+    apis:['index.js']
+}
+const swaggerDocs = swaggerJDDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
